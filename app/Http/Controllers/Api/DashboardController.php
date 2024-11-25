bad9f6e331f37d038e28e1dc5d054aa40399d65b
josheff <areejsz124@gmail.com>
2024-11-25 21:39:48 +0900

    dashboard api

---
+++ app/Http/Controllers/Api/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\BaseApiController as BaseApiController;
+use App\Models\Aprobacion;
+use App\Models\Candidato;
+use App\Models\Empresa;
+use App\Models\Evaluacion;
+use App\Models\Perfil;
+use App\Models\PerfilEvaluacion;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends BaseApiController
+{
+    public function index()
+    {
+        $data['candidatos'] = Candidato::when(!Auth::user()->hasRole(['administrador', 'evaluador']), function ($query) {
+            return $query->where('empresa_id', Auth::user()->perfil->empresa->id);
+        })->count();
+        $data['clientes'] = Empresa::count();
+        $data['evaluaciones'] = Evaluacion::when(!Auth::user()->hasRole(['administrador', 'evaluador']), function ($query) {
+            return $query->where('empresa_id', Auth::user()->perfil->empresa->id);
+        })->count();
+        $data['perfiles'] = PerfilEvaluacion::count();
+        $data['evaluacionesPorPerfil'] = Evaluacion::when(!Auth::user()->hasRole(['administrador', 'evaluador']), function ($query) {
+            return $query->where('empresa_id', Auth::user()->perfil->empresa->id);
+        })->with('perfilEvaluacion')->select('perfil_evaluacion_id', DB::raw('count(*) as evaluaciones_count'))->groupBy('perfil_evaluacion_id')->get()->pluck('evaluaciones_count', 'perfilEvaluacion.nombre');
+        $data['aprobaciones'] = Aprobacion::when(!Auth::user()->hasRole(['administrador', 'evaluador']), function ($query) {
+            return $query->whereRelation('evaluacion', 'empresa_id', Auth::user()->perfil->empresa->id);
+        })->select('estado', DB::raw('count(*) as count'))->groupBy('estado')->pluck('count');
+        return $this->sendResponse($data, 'Fetched data successfully');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+});
+Route::fallback(function(){
+    return response()->json(['message' => 'Not found'], 404);
 });
